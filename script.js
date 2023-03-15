96186d64e0ebe6d2769b9d68377b9ce49ee6c524
mcr <43091597+mcrombeen@users.noreply.github.com>
2023-03-15 12:14:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     const gameBoard = document.getElementById('game-board');
     const imagePicker = document.getElementById('image-picker');
