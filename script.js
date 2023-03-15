085f040dbcced3c9fcadc1e643b2959914bdcd08
mcr <43091597+mcrombeen@users.noreply.github.com>
2023-03-15 13:22:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,43 @@
-function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-}
-
 document.addEventListener('DOMContentLoaded', () => {
     const gameBoard = document.getElementById('game-board');
     const imagePicker = document.getElementById('image-picker');
     const tiles = [];
+    let emptyIndex;
 
     function createTiles() {
-        for (let i = 0; i < 16; i++) {
+        for (let i = 0; i < 15; i++) {
             const tile = document.createElement('div');
             tile.className = 'tile';
             tile.addEventListener('click', onTileClick);
             tiles.push(tile);
             gameBoard.appendChild(tile);
         }
+
+        const emptyTile = document.createElement('div');
+        emptyTile.className = 'tile empty';
+        emptyTile.style.backgroundColor = 'lightblue';
+        tiles.push(emptyTile);
+        gameBoard.appendChild(emptyTile);
+        emptyIndex = 15;
     }
 
     function onTileClick(event) {
-    const clickedTile = event.target;
-    if (clickedTile.classList.contains('empty-tile')) return; // Ignore click on the blue tile
+        const clickedTile = event.target;
+        if (clickedTile === tiles[emptyIndex]) {
+            return;
+        }
 
-    const tileIndex = tiles.indexOf(clickedTile);
-    const emptyTile = tiles.find((tile) => tile.classList.contains('empty-tile'));
-    const emptyIndex = tiles.indexOf(emptyTile);
+        const clickedIndex = tiles.indexOf(clickedTile);
+        const isAdjacent = Math.abs(emptyIndex - clickedIndex) === 1 || Math.abs(emptyIndex - clickedIndex) === 4;
 
-    function isTileAdjacent() {
-        return Math.abs(emptyIndex - tileIndex) === 1 || Math.abs(emptyIndex - tileIndex) === 4;
+        if (isAdjacent) {
+            gameBoard.insertBefore(tiles[emptyIndex], tiles[clickedIndex]);
+            gameBoard.insertBefore(tiles[clickedIndex], tiles[emptyIndex]);
+
+            [tiles[emptyIndex], tiles[clickedIndex]] = [tiles[clickedIndex], tiles[emptyIndex]];
+            emptyIndex = clickedIndex;
+        }
     }
-
-    if (isTileAdjacent()) {
-        const tempStyle = clickedTile.getAttribute('style');
-        clickedTile.setAttribute('style', emptyTile.getAttribute('style'));
-        emptyTile.setAttribute('style', tempStyle);
-
-        // Swap the positions in the tiles array
-        [tiles[tileIndex], tiles[emptyIndex]] = [tiles[emptyIndex], tiles[tileIndex]];
-    }
-}
-
 
     function handleImageSelection() {
         const file = imagePicker.files[0];
@@ -68,21 +64,12 @@ document.addEventListener('DOMContentLoaded', () => {
                 });
             }
 
-            // Remove one of the positions for the empty space and set the blue background
-            const emptyIndex = Math.floor(Math.random() * positions.length);
-            positions.splice(emptyIndex, 1);
-            tiles[emptyIndex].classList.add('empty-tile');
-            tiles[emptyIndex].style.backgroundColor = 'lightblue';
+            positions.splice(Math.floor(Math.random() * positions.length), 1);
 
-            shuffleArray(positions);
-
-            for (let i = 0, j = 0; i < 16; i++) {
-                if (i !== emptyIndex) {
-                    tiles[i].style.backgroundImage = `url('${imageSrc}')`;
-                    tiles[i].style.backgroundSize = `${tempImage.width}px ${tempImage.height}px`;
-                    tiles[i].style.backgroundPosition = `-${positions[j].x}px -${positions[j].y}px`;
-                    j++;
-                }
+            for (let i = 0; i < 15; i++) {
+                tiles[i].style.backgroundImage = `url('${imageSrc}')`;
+                tiles[i].style.backgroundSize = `${tempImage.width}px ${tempImage.height}px`;
+                tiles[i].style.backgroundPosition = `-${positions[i].x}px -${positions[i].y}px`;
             }
         };
     }
