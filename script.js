bf89fe4923e6f97429c31378a43e09c59e634d5b
mcr <43091597+mcrombeen@users.noreply.github.com>
2023-03-15 11:49:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-}
-
 document.addEventListener('DOMContentLoaded', () => {
     const gameBoard = document.getElementById('game-board');
     const imagePicker = document.getElementById('image-picker');
@@ -23,29 +16,26 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function startDragging(event) {
-    const draggedTile = event.target;
-    const emptyTile = gameBoard.children[15];
+        const draggedTile = event.target;
+        const emptyTile = gameBoard.children[15];
 
-    function isTileAdjacent() {
-        const emptyIndex = Array.prototype.indexOf.call(gameBoard.children, emptyTile);
-        const tileIndex = Array.prototype.indexOf.call(gameBoard.children, draggedTile);
-        return Math.abs(emptyIndex - tileIndex) === 1 || Math.abs(emptyIndex - tileIndex) === 4;
-    }
-
-    function moveTile() {
-        if (isTileAdjacent()) {
-            gameBoard.insertBefore(draggedTile, gameBoard.children[Array.prototype.indexOf.call(gameBoard.children, emptyTile)]);
+        function isTileAdjacent() {
+            const emptyIndex = Array.prototype.indexOf.call(gameBoard.children, emptyTile);
+            const tileIndex = Array.prototype.indexOf.call(gameBoard.children, draggedTile);
+            return Math.abs(emptyIndex - tileIndex) === 1 || Math.abs(emptyIndex - tileIndex) === 4;
         }
-        document.removeEventListener('mousemove', moveTile);
-        document.removeEventListener('mouseup', moveTile);
-    }
 
-    if (isTileAdjacent()) {
-        document.addEventListener('mousemove', moveTile);
-        document.addEventListener('mouseup', moveTile);
-    }
-}
+        function moveTile() {
+            if (isTileAdjacent()) {
+                gameBoard.insertBefore(draggedTile, gameBoard.children[Array.prototype.indexOf.call(gameBoard.children, emptyTile)]);
+            }
+        }
 
+        draggedTile.addEventListener('mouseup', () => {
+            moveTile();
+            draggedTile.removeEventListener('mouseup', moveTile);
+        });
+    }
 
     function handleImageSelection() {
         const file = imagePicker.files[0];
@@ -58,37 +48,35 @@ document.addEventListener('DOMContentLoaded', () => {
         };
     }
 
-   function applyUserImage(imageSrc) {
-    const tempImage = new Image();
-    tempImage.src = imageSrc;
-    tempImage.onload = () => {
-        const tileSize = tempImage.width / 4;
-        const positions = [];
+    function applyUserImage(imageSrc) {
+        const tempImage = new Image();
+        tempImage.src = imageSrc;
+        tempImage.onload = () => {
+            const tileSize = tempImage.width / 4;
+            const positions = [];
 
-        for (let i = 0; i < 16; i++) {
-            positions.push({
-                x: (i % 4) * tileSize,
-                y: Math.floor(i / 4) * tileSize
-            });
-        }
+            for (let i = 0; i < 16; i++) {
+                positions.push({
+                    x: (i % 4) * tileSize,
+                    y: Math.floor(i / 4) * tileSize
+                });
+            }
 
-        // Remove one of the positions for the empty space
-        positions.splice(Math.floor(Math.random() * positions.length), 1);
+            // Remove one of the positions for the empty space
+            positions.splice(Math.floor(Math.random() * positions.length), 1);
 
-        shuffleArray(positions);
+            shuffleArray(positions);
 
-        for (let i = 0; i < 15; i++) {
-            tiles[i].style.backgroundImage = `url('${imageSrc}')`;
-            tiles[i].style.backgroundSize = `${tempImage.width}px ${tempImage.height}px`;
-            tiles[i].style.backgroundPosition = `-${positions[i].x}px -${positions[i].y}px`;
-        }
-    };
-}
-
+            for (let i = 0; i < 15; i++) {
+                tiles[i].style.backgroundImage = `url('${imageSrc}')`;
+                tiles[i].style.backgroundSize = `${tempImage.width}px ${tempImage.height}px`;
+                tiles[i].style.backgroundPosition = `-${positions[i].x}px -${positions[i].y}px`;
+            }
+        };
+    }
 
     createTiles();
     imagePicker.addEventListener('change', handleImageSelection);
 });
-
-                   
+  
 
