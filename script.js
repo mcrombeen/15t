c431ec9039c3cbbc4196c8fbf4d3ac9741d63ab0
mcr <43091597+mcrombeen@users.noreply.github.com>
2023-03-16 16:36:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
-const presetImages = [
-    'https://raw.githubusercontent.com/mcrombeen/15t/main/1.jpg',
-    'https://raw.githubusercontent.com/mcrombeen/15t/main/2.jpg'
-];
-
 document.addEventListener('DOMContentLoaded', () => {
     const gameBoard = document.getElementById('game-board');
     const imagePicker = document.getElementById('image-picker');
     const tiles = [];
 
-    const defaultImageURL = 'https://raw.githubusercontent.com/mcrombeen/15t/main/oie_oie_canvas.png';
+    const presetImages = ['assets/1.jpg', 'assets/2.jpg'];
 
     const btnImagePicker = document.getElementById('btn-image-picker');
     const hiddenImagePicker = document.createElement('input');
@@ -32,7 +27,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
         const yellowTile = document.createElement('div');
         yellowTile.className = 'tile yellow-tile';
-        yellowTile.style.backgroundImage = "url('https://raw.githubusercontent.com/mcrombeen/15t/main/test1/oie_oie_canvas.png')";
+        yellowTile.style.backgroundImage = "url('https://raw.githubusercontent.com/mcrombeen/15t/main/test1/oie_oie_canvas.png')"; // Replace this path with the path to your yellow tile image
         yellowTile.style.backgroundSize = 'cover';
         gameBoard.appendChild(yellowTile);
     }
@@ -41,7 +36,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const clickedTile = event.target;
         const yellowTile = document.querySelector('.yellow-tile');
         if (isAdjacent(clickedTile, yellowTile)) {
-            swapTiles(clickedTile, yellowTile);
+            animateSwapTiles(clickedTile, yellowTile);
         }
     }
 
@@ -51,25 +46,54 @@ document.addEventListener('DOMContentLoaded', () => {
         return Math.abs(index1 - index2) === 1 || Math.abs(index1 - index2) === 4;
     }
 
-    function swapTiles(tile1, tile2) {
-        const dx = tile1.offsetLeft - tile2.offsetLeft;
-        const dy = tile1.offsetTop - tile2.offsetTop;
+    function animateSwapTiles(tile1, tile2) {
+        const tile1Rect = tile1.getBoundingClientRect();
+        const tile2Rect = tile2.getBoundingClientRect();
 
-        tile1.style.transform = `translate(${dx}px, ${dy}px)`;
-        tile2.style.transform = `translate(${-dx}px, ${-dy}px)`;
+        const tile1Clone = tile1.cloneNode(true);
+        const tile2Clone = tile2.cloneNode(true);
 
-        tile1.addEventListener('transitionend', function onTransitionEnd() {
-            tile1.removeEventListener('transitionend', onTransitionEnd);
+        tile1.style.visibility = 'hidden';
+        tile2.style.visibility = 'hidden';
 
-            const temp = document.createElement('div');
-            gameBoard.insertBefore(temp, tile1);
-            gameBoard.insertBefore(tile1, tile2);
-            gameBoard.insertBefore(tile2, temp);
-            gameBoard.removeChild(temp);
+        tile1Clone.style.position = 'absolute';
+        tile1Clone.style.left = tile1Rect.left + 'px';
+        tile1Clone.style.top = tile1Rect.top + 'px';
 
-            tile1.style.transform = '';
-            tile2.style.transform = '';
+        tile2Clone.style.position = 'absolute';
+        tile2Clone.style.left = tile2Rect.left + 'px';
+        tile2Clone.style.top = tile2Rect.top + 'px';
+
+        document.body.appendChild(tile1Clone);
+        document.body.appendChild(tile2Clone);
+
+        const deltaX = tile1Rect.left - tile2Rect.left;
+        const deltaY = tile1Rect.top - tile2Rect.top;
+
+        requestAnimationFrame(() => {
+            tile1Clone.style.transition = 'transform 300ms';
+            tile2Clone.style.transition = 'transform 300ms';
+
+            tile1Clone.style.transform = `translate(${-deltaX}px, ${-deltaY}px)`;
+            tile2Clone.style.transform = `translate(${deltaX}px, ${deltaY}px)`;
+
+            setTimeout(() => {
+                swapTiles(tile1, tile2);
+                tile1.style.visibility = 'visible';
+                tile2.style.visibility = 'visible';
+
+                document.body.removeChild(tile1Clone);
+                document.body.removeChild(tile2Clone);
+            },300);
         });
+    }
+
+    function swapTiles(tile1, tile2) {
+        const temp = document.createElement('div');
+        gameBoard.insertBefore(temp, tile1);
+        gameBoard.insertBefore(tile1, tile2);
+        gameBoard.insertBefore(tile2, temp);
+        gameBoard.removeChild(temp);
     }
 
     function handleImageSelection() {
@@ -93,40 +117,40 @@ document.addEventListener('DOMContentLoaded', () => {
             for (let i = 0; i < 16; i++) {
                 positions.push({
                     x: (i % 4) * tileSize,
-                    y: Math.floor(i / 4) * tileSize            });
+                    y: Math.floor(i / 4) * tileSize
+                });
+            }
 
-        // Remove one of the positions for the yellow space
-        positions.splice(Math.floor(Math.random() * positions.length), 1);
+            positions.splice(Math.floor(Math.random() * positions.length), 1);
+            shuffleArray(positions);
 
-        shuffleArray(positions);
+            for (let i = 0; i < 15; i++) {
+                tiles[i].style.backgroundImage = `url('${imageSrc}')`;
+                tiles[i].style.backgroundSize = `${tempImage.width}px ${tempImage.height}px`;
+                tiles[i].style.backgroundPosition = `-${positions[i].x}px -${positions[i].y}px`;
+            }
 
-        for (let i = 0; i < 15; i++) {
-            tiles[i].style.backgroundImage = `url('${imageSrc}')`;
-            tiles[i].style.backgroundSize = `${tempImage.width}px ${tempImage.height}px`;
-            tiles[i].style.backgroundPosition = `-${positions[i].x}px -${positions[i].y}px`;
-        }
-
-        const gameBoardBackground = `linear-gradient(rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5)), url('${imageSrc}')`;
-        gameBoard.style.backgroundImage = gameBoardBackground;
-        gameBoard.style.backgroundSize = 'cover';
-    };
-}
-
-function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
+            const gameBoardBackground = `linear-gradient(rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5)), url('${imageSrc}')`;
+            gameBoard.style.backgroundImage = gameBoardBackground;
+            gameBoard.style.backgroundSize = 'cover';
+        };
     }
-}
 
-function getRandomPresetImage() {
-    const index = Math.floor(Math.random() * presetImages.length);
-    return presetImages[index];
-}
+    function shuffleArray(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+    }
 
-createTiles();
-applyUserImage(getRandomPresetImage());
-imagePicker.addEventListener('change', handleImageSelection);
+    function loadRandomPresetImage() {
+        const randomIndex = Math.floor(Math.random() * presetImages.length);
+        const imageURL = presetImages[randomIndex];
+        applyUserImage(imageURL);
+    }
 
-        
-        });
+    createTiles();
+    loadRandomPresetImage();
+    imagePicker.addEventListener('change', handleImageSelection);
+});
+ 
